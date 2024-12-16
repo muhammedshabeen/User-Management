783d5917ccd8bd54ca611442729f43a8ad05bc02
muhammedshabeen <muhammedshabeen982@gmail.com>
2024-12-16 10:15:01 +0530

    urls comments added

--- user_management/urls.py
+++ user_management/urls.py
@@ -3,9 +3,18 @@ from .views import *
 
 urlpatterns = [
     
+    # Route for the login page (default route)
     path('',user_login,name='user_login'),
+    
+    # Route for the registration page
     path('register',register,name='register'),
+    
+    # Route for the dashboard page
     path('dashboard',dashboard,name='dashboard'),
+    
+    # Route for logging out
     path('signout',signout,name='signout'),
+    
+    # Route for the profile page
     path('profile',profile,name='profile'),
 ]
